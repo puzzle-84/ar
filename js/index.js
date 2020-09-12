5b10c531d685ec84c3bdbaf60592e067164eb2b2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-09-12 19:50:40 +0300

    refactored

--- js/index.js
+++ js/index.js
@@ -1,38 +1,31 @@
-const coordinates = [
-    { lat: 53.850547, long: 27.491427 },
-    { lat: 53.850547, long: 27.491427 },
-    { lat: 53.850547, long: 27.491427, text: 'test' },
-];
 window.onload = () => {
-    navigator.geolocation.getCurrentPosition(success);
-    function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const delta = document.querySelector('#delta').value;
-
-        let places = staticLoadPlaces(coordinates);
-        renderPlaces(places, 53.850547, 27.491427);
-        function staticLoadPlaces(coordinates) {
-            return coordinates.map((i, index) => [
-                {
-                    name: 'Gift' + index,
-                    location: {
-                        lat: i.lat,
-                        lng: i.long,
-                    },
+    const coordinates = [
+        { lat: 53.850547, long: 27.491427 },
+        { lat: 53.850547, long: 27.491427 },
+        { lat: 53.850547, long: 27.491427, text: 'test' },
+    ];
+    let places = staticLoadPlaces(coordinates);
+    renderPlaces(places);
+    function staticLoadPlaces(coordinates) {
+        return coordinates.map((i, index) => [
+            {
+                name: 'Gift' + index,
+                location: {
+                    lat: i.lat,
+                    lng: i.long,
                 },
-            ]);
-        }
+            },
+        ]);
     }
 };
 
-function renderPlaces(places, latitude, longitude) {
+function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
     places.forEach((place) => {
         let model = document.createElement('a-entity');
         model.setAttribute(
             'gps-entity-place',
-            `latitude: ${latitude}; longitude: ${longitude};`,
+            `latitude: ${place.location.lat}; longitude: ${place.location.lng};`,
         );
         model.setAttribute('animation-mixer', '');
         scene.appendChild(model);
